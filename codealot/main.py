95a18cb4bb09d73cb16168d061432c840c4b9cce
ShoyonK <shoyon.kermany@utexas.edu>
2022-06-02 20:55:23 -0500

    added ability to simulate longer periods of time

--- codealot/main.py
+++ codealot/main.py
@@ -4,22 +4,29 @@ from Codalot import Codalot
 
 if __name__ == "__main__":
     
+    numberOfDays = int(input('How many days to simulate? '))
+    numberOfHours = numberOfDays*24
     codalot = Codalot(12) #generate Codalot with 12 knights inside
     knights = codalot.getKnights()
-
     random = Random(1)
 
     hour = 0
-    while(hour < 24): #iterate through 24 hours with 1 hour increment
+    while(hour < numberOfHours): #iterate through the number of hours inputted with 1 hour increments
         for knight in knights:
             randomVal = random.randint(0, 1)
             if randomVal == 0:
                 knight.moveToTrainingYard()
             elif randomVal == 1:
                 knight.moveToTavern()
+            if hour % 24 == 0: #every 24 hours belowZeroStaminaFlag
+                knight.setBelowZeroStaminaFlag(False)
+        if hour % 24 == 0: #every 24 hours clear the bonusKnights in the list
+            codalot.clearBonusKnights() 
         codalot.process() #make necessary changes to stamina and xp
         hour = hour + 1
-        
+
+            
+
     codalot.grantBonusXp()
     totalXp = codalot.calculateEarnedXp()
 
